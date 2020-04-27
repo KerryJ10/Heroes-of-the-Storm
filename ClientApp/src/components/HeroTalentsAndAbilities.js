78503bb4a771da58cfec3c04ca3c18bfa10e2bd9
Kerry Johnson <kerryj1010@gmail.com>
2020-04-27 16:02:08 -0500

    Added toggle functionality to Hero Talents and Abilities component

--- ClientApp/src/components/HeroTalentsAndAbilities.js
+++ ClientApp/src/components/HeroTalentsAndAbilities.js
@@ -7,14 +7,36 @@ export class HeroTalentsAndAbilities extends Component {
 	constructor(props) {
 		super(props);
 		this.hero = props.location.hero;
+		this.state = {
+			visibility: true
+		};
+	}
+
+	 testFunc = () => {
+		debugger;
+		if (this.state.visibility) {
+			return (
+				<div>
+					<h1 style={{ visibility: this.state.visibility ? 'visible' : 'hidden' }}  >Abilities</h1>
+				</div>
+			);
+		} else {
+			return (
+				<div>
+					<h1 style={{ visibility: !this.state.visibility ? 'visible' : 'hidden' }}>Talents</h1>
+				</div>
+			);
+		}
 	}
 
 	render() {
+		const contents = this.testFunc();
+
 		return (
 			<div className='hero-t-a-container'>
-				<h1>Talent and Abilities coming soon</h1>
-				<h1>Name: {this.hero.name}</h1>
-				<h1>Hero ID: {this.hero.id}</h1>
+				{contents}
+				<button style={{ backgroundColor: this.state.visibility ? 'gray' : 'white' }} onClick={() => this.setState({ visibility: true })}>Abilities</button>
+				<button style={{ backgroundColor: !this.state.visibility ? 'gray' : 'white' }} onClick={() => this.setState({ visibility: false })}>Talents</button>
 			</div>
 		);
 	}
